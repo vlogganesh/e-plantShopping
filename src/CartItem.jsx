1e4315bd58f1ee6da136c261e2e8957c31166409
vlogganesh <vlogganesh@gmail.com>
2025-01-03 10:17:30 -0500

    Start worling on Task3 step4

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -16,10 +16,11 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-       
+    onContinueShopping();           
   };
-
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');    
+  };
 
   const handleIncrement = (item) => {
   };
