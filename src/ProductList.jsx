60872fa6c4d0eec57889dbd773fd54706bf667f7
vlogganesh <vlogganesh@gmail.com>
2025-01-03 09:47:58 -0500

    Task2 completed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -243,17 +243,17 @@ const handlePlantsClick = (e) => {
     setShowCart(false); // Hide the cart when navigating to About Us
 };
 
-   const handleContinueShopping = (e) => {
+const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
-  };
-  const handleAddToCart = (product) => {
+};
+const handleAddToCart = (product) => {
     dispatch(addItem(product));
     setAddedToCart((prevState) => ({
        ...prevState,
        [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
      }));
-  };
+};
     return (
         <div>
              <div className="navbar" style={styleObj}>
